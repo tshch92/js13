66bd64211697d1f7120d99fe24803894361a59aa
tshch92 <tshch92@gmail.com>
2021-07-08 22:13:11 +0300

    list creation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 'use strict';
 
+const example = [1, 2, 3, ['a', 'b', 'c', [55, 66, 77]], 4];
+
 const tableWidth = 10;
 const tableHeight = 10;
+
+//creating a list
+
+const $myList = document.querySelector('.my-list');
+
+function cascade(arr) {
+    let $ul = document.createElement('ul');
+
+    arr.forEach(element => {
+        let $li = document.createElement('li');
+
+        if (Array.isArray(element)) {
+            $li.innerHTML += cascade(element);
+        } else {
+            $li.textContent = element;
+        }
+
+        $ul.innerHTML += $li.outerHTML;
+    });
+
+    return $ul.outerHTML;
+}
+
+$myList.innerHTML += cascade(example);
+
+//creating a table 
 
 const $myTable = document.querySelector('.my-table');
 
@@ -13,8 +41,9 @@ for (let i = 0; i < tableHeight; i++) {
     let $tblRow = document.createElement('tr');
 
     let $tblCell = document.createElement('td');
+
     for (let j = 0; j < tableWidth; j++) {
-        $tblCell.textContent = i * tableWidth + j;
+        $tblCell.textContent = i * tableWidth + j + 1;
         $tblRow.innerHTML += $tblCell.outerHTML;
     }
 
@@ -24,5 +53,3 @@ for (let i = 0; i < tableHeight; i++) {
 $tbl.innerHTML += $tblBody.outerHTML;
 
 $myTable.innerHTML += $tbl.outerHTML;
-
-console.log($myTable);
